ef5ecd1628a8c1016830a9e4c170e6092c413e0d
Shaurya-Jain <Shaurya.jain@gihs.sa.edu.au>
2017-09-20 16:13:02 +0930

    Add files via upload

---
+++ timetable.py
@@ -0,0 +1,21 @@
+def timesTables():
+    for j in range(1, 13):
+        for i in range(1, 13):
+            print(i*j, end=' ')
+        print()
+
+timesTables()
+
+        
+
+
+
+    
+
+
+
+
+    
+
+
+        
